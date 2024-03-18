297560a24fd5d883676287a985b74ea88cf3581f
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2024-03-17 21:34:53 -0500

    add bind components.

--- svelte-course/src/lib/components/each/Each.svelte
+++ svelte-course/src/lib/components/each/Each.svelte
@@ -2,11 +2,20 @@
   import TodoList from "./TodoList.svelte";
   import { v4 as uuid } from "uuid";
 
-  const todos = [
+  let todos = [
     { id: uuid(), title: "Todo 1", completed: true },
     { id: uuid(), title: "Todo 2", completed: false },
-    { id: uuid(), title: "Todo 3", completed: true }
+    { id: uuid(), title: "Todo 3", completed: true },
   ];
+
+  $: console.log(todos);
 </script>
 
-<TodoList {todos}/>
+<h2>{todos.length} Todos</h2>
+
+<button
+  on:click={() => {
+    todos = [];
+  }}>Update todo</button
+>
+<TodoList bind:todos />
