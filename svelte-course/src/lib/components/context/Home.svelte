00a45a23b2e1e3d0afd8f86cc059eb66716b8796
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2024-03-27 21:27:42 -0500

    Carry out context component.

--- svelte-course/src/lib/components/context/Home.svelte
+++ svelte-course/src/lib/components/context/Home.svelte
@@ -1,66 +1,32 @@
 <script>
-  let values = { username: "", email: "", password: "" };
-  let isSubmitting = false;
-  let errors = {};
-
-  function validate() {
-    const errors = {};
-
-    if (!values.username) {
-      errors.username = "The username is required.";
-    }
-
-    if (!values.password) {
-      errors.password = "The password is required";
-    }
-
-    if (values.email) {
-      errors.email = "The email is invalid";
-    }
-
-    return errors;
-  }
+  import {
+    validateEmail,
+    validateRequiredField,
+  } from "./../utils/validation.js";
+  import Form from "./../form/Form.svelte";
+  import Field from "./../form/Field.svelte";
 </script>
 
-<form
-  on:submit|preventDefault={() => {
-    errors = validate();
-
-    if (Object.keys(errors).length === 0) {
-      isSubmitting = true;
-      setTimeout(() => {
-        isSubmitting = false;
-      }, 1000);
-    }
+<Form
+  on:submit={(e) => {
+    console.log(e.detail);
   }}
+  initialValues={{ username: "Test", email: "test@test.com" }}
 >
-  <label for="username">username: </label><br />
-  <input
-    id="username"
+  <Field
+    label="Username"
     name="username"
     type="text"
-    bind:value={values.username}
-  /> <br />
-  {#if errors.username}
-    <p>{errors.username}</p>
-  {/if}
-  <label for="email">email: </label><br />
-  <input id="email" name="email" type="email" bind:value={values.email} />
-  <br />
-  {#if errors.email}
-    <p>{errors.email}</p>
-  {/if}
-
-  <label for="password">password: </label><br />
-  <input
-    id="password"
-    name="password"
-    type="password"
-    bind:value={values.password}
-  /> <br />
-  {#if errors.password}
-    <p>{errors.password}</p>
-  {/if}
-
-  <button type="submit" disabled={isSubmitting}>Submit</button>
-</form>
+    validate={validateRequiredField}
+  />
+  <Field
+    label="Email"
+    name="email"
+    type="email"
+    validate={(value, label) => {
+      return validateRequiredField(value, label) || validateEmail(value, label);
+    }}
+  />
+  <Field label="Password" name="password" type="password" validate={validateRequiredField} />
+  <button type="submit">Submit</button>
+</Form>
